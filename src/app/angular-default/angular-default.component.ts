112689147406574f4d33585246ddac8a65c3c3b8
sckim <sckim@wins21.co.kr>
2021-01-27 08:01:25 +0900

    fix warnings

--- src/app/angular-default/angular-default.component.ts
+++ src/app/angular-default/angular-default.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class AngularDefaultComponent implements OnInit {
 
   constructor() { }
-
+  title = 'angularTodo';
   ngOnInit(): void {
   }
 
